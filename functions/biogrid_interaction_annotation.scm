8916adcdf9f23a56c5a5375f7fdc5a7c576b537a
hedra <tanksh24@gmail.com>
2019-02-04 23:30:20 -0500

    Updated todo PP interaction if pathway annotation selected

--- functions/biogrid_interaction_annotation.scm
+++ functions/biogrid_interaction_annotation.scm
@@ -1,25 +1,33 @@
+
+
 (define (biogrid_interaction_annotation)
-    (set! result (list (ConceptNode "biogrid_interaction_annotation")))
+    	(define gen_int '())
+    	(set! result (list (ConceptNode "biogrid_interaction_annotation")))
     
     (for-each (lambda (gene)
+	(set! gen_int (cog-outgoing-set (findGeneInteractor gene)))
+
+	(if (equal? interaction "proteins")
+	(for-each (lambda(pro)
+		(for-each (lambda (int)
+			(for-each (lambda (int_prot)
+				(set! result (append result 
+				(list (EvaluationLink (PredicateNode "interacts_with") (ListLink pro int_prot)))))
+			) (cog-outgoing-set (findprotein int)))
+		) gen_int)
+        )(cog-outgoing-set (findprotein gene))))
+
+
+	(if (equal? interaction "genes")
         (for-each (lambda (g)
             (set! result (append result 
                     (list (EvaluationLink (PredicateNode "interacts_with") (ListLink gene g)))))
-                    )(cog-outgoing-set (findGeneInteractor gene)))
-    ) gene_nodes)
- 
+                    )gen_int))
+	 
+    )gene_nodes)
+
+    ;; TODO: check for resulting genes if interacting to each other
+
   result
 )   
 
-(define findGeneInteractor
-    (lambda(gene)
-        (cog-execute! (GetLink
-            (VariableNode "$a")
-            (EvaluationLink
-               (PredicateNode "interacts_with")
-               (ListLink
-               gene
-               (VariableNode "$a")
-              )
-            )
-    ))))

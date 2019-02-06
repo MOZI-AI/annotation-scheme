bdab849fef6828d152743c5eff24e8a5821bc11b
hedra <tanksh24@gmail.com>
2019-02-06 18:22:06 -0500

    Output genes interacting eachother are included in the result

--- functions/biogrid_interaction_annotation.scm
+++ functions/biogrid_interaction_annotation.scm
@@ -1,5 +1,4 @@
 
-
 (define (biogrid_interaction_annotation)
     	(define gen_int '())
     	(set! result (list (ConceptNode "biogrid_interaction_annotation")))
@@ -17,17 +16,25 @@
 		) gen_int)
         )(cog-outgoing-set (findprotein gene))))
 
+	(if (equal? interaction "genes") 
 
-	(if (equal? interaction "genes")
-        (for-each (lambda (g)
-            (set! result (append result 
+	    (for-each (lambda (g)
+                (set! result (append result 
                     (list (EvaluationLink (PredicateNode "interacts_with") (ListLink gene g)))))
-                    )gen_int))
+
+                ;; checks for output genes if interacting to each other
+
+	        (let ([remain gen_int])
+    		(set! remain (cdr remain))
+    		(for-each (lambda (r)
+    			(if (member g (cog-outgoing-set (findGeneInteractor r)))
+    				(set! result (append result 
+				(list (EvaluationLink (PredicateNode "interacts_with") (ListLink g r))))))
+   		) remain))
+
+	)gen_int))
 	 
     )gene_nodes)
 
-    ;; TODO: check for resulting genes if interacting to each other
-
   result
-)   
-
+)

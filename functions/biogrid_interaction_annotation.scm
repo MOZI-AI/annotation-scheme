47e2250c0ab0be22cea5fa49756e154791192dc0
hedra <tanksh24@gmail.com>
2019-02-13 22:17:59 -0500

    Code for PPI modified

--- functions/biogrid_interaction_annotation.scm
+++ functions/biogrid_interaction_annotation.scm
@@ -1,39 +1,28 @@
 
 (define (biogrid_interaction_annotation)
     	(define gen_int '())
+	(define prot_int '())
     	(set! result (list (ConceptNode "biogrid_interaction_annotation")))
     
     (for-each (lambda (gene)
-	(set! gen_int (cog-outgoing-set (findGeneInteractor gene)))
 
 	(if (equal? interaction "proteins")
-	(for-each (lambda(pro)
-		(for-each (lambda (int)
-			(for-each (lambda (int_prot)
-				(set! result (append result 
-				(list (EvaluationLink (PredicateNode "interacts_with") (ListLink pro int_prot)))))
-			) (cog-outgoing-set (findprotein int)))
-		) gen_int)
-        )(cog-outgoing-set (findprotein gene))))
+	     (set! result (append result (cog-outgoing-set (findProtInteractor gene)))))
 
 	(if (equal? interaction "genes") 
-
-	    (for-each (lambda (g)
-                (set! result (append result 
-                    (list (EvaluationLink (PredicateNode "interacts_with") (ListLink gene g)))))
-
-                ;; checks for output genes if interacting to each other
-
-	        (let ([remain gen_int])
-    		(set! remain (cdr remain))
-    		(for-each (lambda (r)
+           (begin
+		(set! result (append result (cog-outgoing-set (matchGeneInteractors gene))))
+                ;; check for output genes if interacting to each other
+		(set! gen_int (cog-outgoing-set (findGeneInteractor gene)))
+	        (for-each (lambda (g)
+	            (set! gen_int (cdr gen_int))
+		    (for-each (lambda (r)
     			(if (member g (cog-outgoing-set (findGeneInteractor r)))
     				(set! result (append result 
 				(list (EvaluationLink (PredicateNode "interacts_with") (ListLink g r))))))
-   		) remain))
+   		    ) gen_int)
+		)(cog-outgoing-set (findGeneInteractor gene)))))
 
-	)gen_int))
-	 
     )gene_nodes)
 
   result

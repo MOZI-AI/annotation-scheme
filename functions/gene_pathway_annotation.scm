c3cb42dbd238d4142f5838df93407bf8a30eb7d9
tanksha <tanksh24@gmail.com>
2019-03-18 15:41:14 -0400

    Added info about proteins, pathways and molecules

--- functions/gene_pathway_annotation.scm
+++ functions/gene_pathway_annotation.scm
@@ -25,14 +25,14 @@
   (define pw '())
   (set! pw (identify_pw gene pw "SMP"))
   (for-each (lambda(path)
-        (set! result (append result (list (MemberLink gene path))))
+        (set! result (append result (list (list (MemberLink gene path) (node-info path)))))
         (if (equal? sm "True")
             (filter_by (cog-outgoing-set (findmol path)) "ChEBI:" path))
       )pw)
   (if (equal? prot "True")
       (for-each (lambda(pro)
             (set! result (append result
-                    (list (EvaluationLink (PredicateNode "expresses") (ListLink gene pro)))))
+                    (list (list (EvaluationLink (PredicateNode "expresses") (ListLink gene pro)) (node-info pro)))))
                   )(cog-outgoing-set (findprotein gene))))
 )
 
@@ -42,7 +42,7 @@
 (define (filter_by res str path)
 (for-each (lambda (m)
   (if (string-contains (cog-name m) str)
-      (set! result (append result (list (MemberLink m path)))
+      (set! result (append result (list (list (MemberLink m path) (node-info m))))
               )res)
   ) res))
 
@@ -54,19 +54,19 @@
     (set! pw (identify_pw gene pw "R-HSA"))
   
       (for-each (lambda(path)
-        (set! result (append result (list (MemberLink gene path))))
+        (set! result (append result (list (list (MemberLink gene path) (node-info path)))))
 
         (if (equal? prot "True")
             (for-each (lambda (mol)
                 (if (string-contains (cog-name mol) "Uniprot:") 
                 (set! result (append result
-                    (list (MemberLink mol path)))))) (cog-outgoing-set (findmol path))))
+                    (list (list (MemberLink mol path) (node-info mol))))))) (cog-outgoing-set (findmol path))))
             
         (if (equal? small_mol "True")
             (for-each (lambda (smol)
             (if (string-contains (cog-name smol) "ChEBI:") 
             (set! result (append result
-                    (list (MemberLink smol path)))))) (cog-outgoing-set (findmol path))))
+                    (list (list (MemberLink smol path) (node-info smol))))))) (cog-outgoing-set (findmol path))))
       )pw)
   
 )
@@ -81,3 +81,4 @@
               )mem)
   pw
 )
+

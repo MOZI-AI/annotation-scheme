de87589d498cdc00c5210d922e762c0abb0f341e
enku2015 <enku.wende@gmail.com>
2019-06-02 07:30:20 -0500

    Define a test function as the previous one had an error

--- tests/sample_demo.scm
+++ tests/sample_demo.scm
@@ -40,17 +40,17 @@
 
 (define ex1 '())
 
-(if (equal? (genes (list "MAP2K4" "SPAG9")) "0") 
-(begin
-(set! ex1 
- (append (gene-info (mapSymbol (list "MAP2K4" "SPAG9")))
- (list 
-  (gene-go-annotation (list "MAP2K4" "SPAG9") (list "biological_process" "cellular_component"))
-  (gene-pathway-annotation (list "MAP2K4" "SPAG9") "smpdb reactome" "True" "True")
-  (biogrid-interaction-annotation (list "MAP2K4" "SPAG9") "proteins" )
- ))
-)
-))
+;(if (equal? (genes (list "MAP2K4" "SPAG9")) "0")
+;(begin
+;(set! ex1
+; (append (gene-info (mapSymbol (list "MAP2K4" "SPAG9")))
+; (list
+;  (gene-go-annotation (list "MAP2K4" "SPAG9") (list "biological_process cellular_component"))
+;  (gene-pathway-annotation (list "MAP2K4" "SPAG9") "smpdb reactome" "True" "True")
+;  (biogrid-interaction-annotation (list "MAP2K4" "SPAG9") "proteins" )
+; ))
+;)
+;))
 
 
 (call-with-output-file "example-1.scm"
@@ -60,4 +60,12 @@
 ;;(parse ex1 (list "MAP2K4" "SPAG9"))
 
   
-
+(define result (append
+				(list (gene-info (mapSymbol (list "SPAG9" "MAP2K4" ))))
+				(list
+				 ( gene-go-annotation (list "SPAG9" "MAP2K4" )  "biological_process cellular_component molecular_function" 0)
+				 ( gene-pathway-annotation (list "SPAG9" "MAP2K4" )  "reactome"  "True"  "False" )
+				 ( biogrid-interaction-annotation (list "SPAG9" "MAP2K4" )  "Proteins" )
+				)
+			   )
+)

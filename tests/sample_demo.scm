e7bd02b6311ab4fc6c8a76782055b88fe9225426
tanksha <tanksh24@gmail.com>
2019-06-11 12:19:17 -0400

    Example annotation result written to file for debuging

--- tests/sample_demo.scm
+++ tests/sample_demo.scm
@@ -38,34 +38,22 @@
 
 ;; Do some examples
 
-(define ex1 '())
-
-;(if (equal? (genes (list "MAP2K4" "SPAG9")) "0")
-;(begin
-;(set! ex1
-; (append (gene-info (mapSymbol (list "MAP2K4" "SPAG9")))
-; (list
-;  (gene-go-annotation (list "MAP2K4" "SPAG9") (list "biological_process cellular_component"))
-;  (gene-pathway-annotation (list "MAP2K4" "SPAG9") "smpdb reactome" "True" "True")
-;  (biogrid-interaction-annotation (list "MAP2K4" "SPAG9") "proteins" )
-; ))
-;)
-;))
-
-
-(call-with-output-file "example-1.scm"
-(lambda (output-port)
-(display ex1 output-port)))
-
-;;(parse ex1 (list "MAP2K4" "SPAG9"))
-
-  
 (define result (append
 				(list (gene-info (mapSymbol (list "SPAG9" "MAP2K4" ))))
 				(list
 				 ( gene-go-annotation (list "SPAG9" "MAP2K4" )  "biological_process cellular_component molecular_function" 0)
-				 ( gene-pathway-annotation (list "SPAG9" "MAP2K4" )  "reactome"  "True"  "False" )
+				 ( gene-pathway-annotation (list "SPAG9" "MAP2K4" )  "smpdb reactome"  "True"  "True" )
 				 ( biogrid-interaction-annotation (list "SPAG9" "MAP2K4" )  "Proteins" )
 				)
 			   )
 )
+
+
+(call-with-output-file "example-1.scm"
+(lambda (output-port)
+(display result output-port)))
+
+;;(parse ex1 (list "MAP2K4" "SPAG9"))
+
+  
+

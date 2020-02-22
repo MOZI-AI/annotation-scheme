73f34df5280dfa79a7e562c63c77c6032553c981
Linas Vepstas <linasvepstas@gmail.com>
2020-02-21 21:13:50 -0600

    Fix for guile RAM/garbage-collection explosion.
    This implements the for described in issue #105. This fixes the
    original report of the RAM-usage explostion from December 2019.
    
    There are several other optimizations described in #105 but lets
    do this one first.

--- annotation/functions.scm
+++ annotation/functions.scm
@@ -349,45 +349,30 @@ translates to."
     ((name . rest) name)))
 
 (define-public (pathway-hierarchy pw lst)
-  "Find hierarchy of the reactome pathway."
-  (let ([res-parent
-         (run-query (BindLink
-                     (VariableNode "$parentpw")
-                     (InheritanceLink
-                      pw
-                      (VariableNode "$parentpw"))
-                     (ExecutionOutputLink
-                      (GroundedSchemaNode "scm: check-pathway")
-                      (ListLink
-                       pw
-                       (VariableNode "$parentpw")
-                       (ListLink lst)))))]
-        [res-child
-         (run-query (BindLink
-                     (VariableNode "$parentpw")
-                     (InheritanceLink
-                      (VariableNode "$parentpw")
-                      pw)
-                     (ExecutionOutputLink
-                      (GroundedSchemaNode "scm: check-pathway")
-                      (ListLink
-                       (VariableNode "$parentpw")
-                       pw
-                       (ListLink lst)))))])
-    (append res-parent res-child)))
+" pathway-hierarchy -- Find hierarchy of the reactome pathway."
 
-(define-public check-pathway
-  (lambda (pw parent-pw lst)
-    (if (and (member parent-pw (cog-outgoing-set lst)) (member pw (cog-outgoing-set lst)))
-    (ListLink
-      (InheritanceLink
-      pw
-      parent-pw)
-    ))
-))
+   (define parents (run-query
+      (Get (VariableNode "$parentpw")
+         (Inheritance pw (Variable "$parentpw")))))
 
-;; Finds molecules (proteins or chebi's) in a pathway 
+   (define childs (run-query
+      (Get (VariableNode "$childpw")
+         (Inheritance (Variable "$childpw") pw))))
+
+   (define (check-pathway pw parent-pw lst)
+      (if (and (member parent-pw lst) (member pw lst))
+         (Inheritance pw parent-pw)))
+
+   (define res-parent
+       (map (lambda (parent-pw) (check-pathway pw parent-pw lst)) parents))
+
+   (define res-child
+      (map (lambda (child-pw) (check-pathway child-pw pw lst)) childs)]
+
+   (append res-parent res-child)))
+
 (define-public (find-mol path identifier)
+" Finds molecules (proteins or chebi's) in a pathway"
   (run-query (BindLink
     (TypedVariable (Variable "$a") (TypeNode 'MoleculeNode))
     (AndLink

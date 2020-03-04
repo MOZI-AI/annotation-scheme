180a6e7afb4ec94fda7a1594e87cc62deb31f9ec
Linas Vepstas <linasvepstas@gmail.com>
2020-03-04 12:19:57 -0600

    nd one more ifx.

--- annotation/gene-pathway.scm
+++ annotation/gene-pathway.scm
@@ -30,7 +30,7 @@
       #:use-module (ice-9 match)
       #:export (gene-pathway-annotation))
 
-(include "instrumentation.scm")
+(include-from-path "annotation/instrumentation.scm")
 
 (define-public gene-path-anno-ctr (accum-time "gene-path-anno"))
 (define-public gene-path-write-ctr (accum-time "gene-path-write"))

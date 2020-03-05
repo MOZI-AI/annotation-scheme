c585cef2bd2037feb58966fb7d23f16860f27bc4
Linas Vepstas <linasvepstas@gmail.com>
2020-03-04 23:50:26 -0600

    Revise report for readability

--- annotation/report.scm
+++ annotation/report.scm
@@ -2,18 +2,14 @@
 (define-public (report)
 " Print profile report... "
 
-	; top-level stuff
-	(biogrid-ctr #:report? #t)
-	(biowrite-ctr #:report? #t)
-	(gene-go-anno-ctr #:report? #t)
-	(gene-go-write-ctr #:report? #t)
+	; Pathway stuff.
+	(format #t "Pathway functions\n")
 	(gene-path-anno-ctr #:report? #t)
 	(gene-path-write-ctr #:report? #t)
 
 	(smpdb-ctr #:report? #t)
 	(reactome-ctr #:report? #t)
 
-	; pathway stuff
 	(find-pathway-genes-ctr #:report? #t)
 	(add-pathway-genes-ctr #:report? #t)
 	(find-pathway-member-ctr #:report? #t)
@@ -21,25 +17,32 @@
 	(generate-interactors-ctr #:report? #t)
 	(pathway-hierarchy-ctr #:report? #t)
 	(find-protein-ctr #:report? #t)
-	(find-mol-ctr #:report? #t)
-	(find-go-term-ctr #:report? #t)
 	
+	; go stuff
+	(format #t "\nGO functions\n")
+	(gene-go-anno-ctr #:report? #t)
+	(gene-go-write-ctr #:report? #t)
+	(find-go-term-ctr #:report? #t)
+	(add-go-info-ctr #:report? #t)
+
 	; grid stuff
+	(format #t "\nBiogrid functions\n")
+	(biogrid-ctr #:report? #t)
+	(biowrite-ctr #:report? #t)
 	(match-gene-interactors-ctr #:report? #t)
 	(find-output-interactors-ctr #:report? #t)
 	(generate-result-ctr #:report? #t)
 	(build-interaction-ctr #:report? #t)
+	(find-memberln-ctr #:report? #t)
 	(find-protein-form-ctr #:report? #t)
-	
-	; common to grid and path
-	(find-name-ctr #:report? #t)
 	(find-pubmed-id-ctr #:report? #t)
 	(find-rna-ctr #:report? #t)
 	
-	; ???
-	(find-memberln-ctr #:report? #t)
-	(add-go-info-ctr #:report? #t)
-	(find-parent-ctr #:report? #t)
+	; common to grid and path
+	(format #t "\nCommon functions\n")
+	(find-mol-ctr #:report? #t)
+	(find-name-ctr #:report? #t)
 	(locate-node-ctr #:report? #t)
 	(add-loc-ctr #:report? #t)
+	(find-parent-ctr #:report? #t)
 )

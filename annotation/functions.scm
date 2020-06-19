de44e770edda3fd0ec9b907d32ded526eb59132a
Abdulrahman Semrie <hsamireh@gmail.com>
2020-06-19 20:14:03 +0300

    Minor fix

--- annotation/functions.scm
+++ annotation/functions.scm
@@ -465,7 +465,7 @@
                            (Evaluation (Predicate "from_organism")
                               (List 
                                  (Variable "$a")
-                                 (ConceptNode (string-append "ncbi:" (number->string org)))
+                                 (ConceptNode (string-append "ncbi:" org))
                               )
                            )
                         )
@@ -505,7 +505,7 @@
                            (Evaluation (Predicate "from_organism")
                               (List 
                                  (Variable "$a")
-                                 (ConceptNode (string-append "ncbi:" (number->string org)))
+                                 (ConceptNode (string-append "ncbi:" org))
                               )
                            )
                         )

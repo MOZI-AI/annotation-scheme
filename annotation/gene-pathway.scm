77769426e729461e8c0f9614332d3e9c5cf20be5
Abdulrahman Semrie <hsamireh@gmail.com>
2020-10-12 11:31:11 +0300

    Use only string db for cross-annotation with pathway

--- annotation/gene-pathway.scm
+++ annotation/gene-pathway.scm
@@ -40,7 +40,7 @@
                                   (pathway "reactome")
                                   (include_sm #t) (gene-level? #f)
                                   (namespace "") (parents 0)
-                                  (biogrid #f) (string #f)
+                                  (string? #f)
                                   (regulates #f) (part-of #f) (bi-dir #f)
                                   (coding #f) (noncoding #f))
 
@@ -53,10 +53,10 @@
         (send-message (ConceptNode "gene-pathway-annotation") chans)
         (for-each (lambda (pathway)
           (let ((pathway-type (if (string=? pathway "reactome") 'ReactomeNode 'SmpNode)))
-            (annotate-pathways node chans pathway-type include_sm gene-level? namespaces parents regulates part-of bi-dir string coding noncoding))) pathways)))))
+            (annotate-pathways node chans pathway-type include_sm gene-level? namespaces parents regulates part-of bi-dir string? coding noncoding))) pathways)))))
 
 
-(define (annotate-pathways node chans pathway-type sm? gene-level? namespaces num-parents regulates part-of bi-dir biogrid coding-rna non-coding-rna)
+(define (annotate-pathways node chans pathway-type sm? gene-level? namespaces num-parents regulates part-of bi-dir string? coding-rna non-coding-rna)
 
   (let* ([pw (find-pathway-member node pathway-type)])
 
@@ -74,7 +74,7 @@
            part-of bi-dir coding-rna non-coding-rna) chans)
       )
      
-      (if string 
+      (if string?
         (if gene-level?
           (send-message (find-pathway/go-gene-interactors path) chans)
           (send-message (find-pathway/go-protein-interactors path) chans)))) pw))
